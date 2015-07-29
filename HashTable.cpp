de658620101807628b8b575981c46e999f970ee5
cincyelite22 <cincyelite22@gmail.com>
2015-07-29 15:16:05 -0400

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -15,47 +15,66 @@
 double getDoubleFromUser(double lowerBound, double higherBound);
 int getIntegerFromUser();
 
+vector<int>* getRandomlyGeneratedVector(unsigned int length)
+{
+	vector<int>* newVector = new vector<int>();
+	for (unsigned int ii = 0; ii < length; ii++)
+	{
+		// <will>
+		// Generate numbers that are in the range [-2 * length, 2 * length)
+		newVector->push_back((rand() % (length * 4)) - length * 2);
+	}
+	return newVector;
+}
+
 int main(int argc, char* argv[])
 {
+    srand(static_cast<unsigned int>(time(NULL)));
+    int totalBuckets = 811;
+    vector<int>* randomNumbers = getRandomlyGeneratedVector( totalBuckets );
+    for( int collisionResolutionScheme = 1; collisionResolutionScheme <= 4; collisionResolutionScheme++){
+    for( double maxLoadRatio = .1; maxLoadRatio <= .86; maxLoadRatio+=.05)
+    {
+        
 	// <will>
 	// get the collision resolution scheme
-	cout << "Select the collision resolution scheme:" << endl;
-	cout << " 1: Linear Probing (Single Hashing)" << endl;
-	cout << " 2: Linear Probing (Double Hashing)" << endl;
-	cout << " 3: Quadratic Probing" << endl;
-	cout << " 4: Seprate Chaining" << endl;
-	cout << " :";
+	//cout << "Select the collision resolution scheme:" << endl;
+	//cout << " 1: Linear Probing (Single Hashing)" << endl;
+	//cout << " 2: Linear Probing (Double Hashing)" << endl;
+	//cout << " 3: Quadratic Probing" << endl;
+	//cout << " 4: Seprate Chaining" << endl;
+	//cout << " :";
 
 	// <will>
 	// get the input
-	int collisionResolutionScheme = 0;
-	cin >> collisionResolutionScheme;
-	if (cin.fail() || collisionResolutionScheme < 1 || collisionResolutionScheme > 4)
-	{
-		return 1;
-	}
+	//int collisionResolutionScheme = 0;
+	//cin >> collisionResolutionScheme;
+	//if (cin.fail() || collisionResolutionScheme < 1 || collisionResolutionScheme > 4)
+	//{
+	//	return 1;
+	//}
 
 	// <will>
 	// get the max load ratio
-	cout << endl << "Enter the max load ratio (0 to 1):" << endl;
-	double maxLoadRatio = getDoubleFromUser(0.0, 1.0);
+	//cout << endl << "Enter the max load ratio (0 to 1):" << endl;
+	//double maxLoadRatio = getDoubleFromUser(0.0, 1.0);
 
 	// <will>
 	// create the hash table
-	int numberOfBuckets = 23;
+	//int numberOfBuckets = 1009;
 	
 	// <alex>
 	// Make our hash table pointer then decide what hash table to create
 	// based on the user input
 	HashTableBase* hashTable;
 	switch(collisionResolutionScheme){
-	    case 1: hashTable = new HashTableLinProbe(numberOfBuckets);
+	    case 1: hashTable = new HashTableLinProbe(totalBuckets);
 	            break;
-	    case 2: hashTable = new HashTableDoubleHash(numberOfBuckets);
+	    case 2: hashTable = new HashTableDoubleHash(totalBuckets);
 	            break;
-	    case 3: hashTable = new HashTableQuadratic(numberOfBuckets);
+	    case 3: hashTable = new HashTableQuadratic(totalBuckets);
 	            break;
-	    case 4: hashTable = new HashTableChaining(numberOfBuckets);
+	    case 4: hashTable = new HashTableChaining(totalBuckets);
                 break;
 	}
 	
@@ -63,16 +82,18 @@ int main(int argc, char* argv[])
 
 	// <will>
 	// have user fill hash table
-	cout << endl << "Enter the numbers one at a time into the hash table:" << endl;
+	//cout << endl << "Enter the numbers one at a time into the hash table:" << endl;
+        int i = 0;
 	while (hashTable->getLoadRatio() < maxLoadRatio)
 	{
-		int newKey = getIntegerFromUser();
-		bool insertSuccess = hashTable->insertKey(newKey);
+		//int newKey = getIntegerFromUser();
+		bool insertSuccess = hashTable->insertKey(randomNumbers->at(i));
 		if (!insertSuccess)
 		{
-			cout << "**"<< newKey << " could not be inserted as it is a duplicate entry." 
-			        << endl;
+			//cout << "**"<< randomNumbers->at(i) << " could not be inserted as it is a duplicate entry." 
+			      //  << endl;
 		}
+                i++;
 	}
 
 	// <will>
@@ -101,13 +122,20 @@ int main(int argc, char* argv[])
 
 	// <will>
 	// print out the hash table to verify
-	cout << endl;
-	hashTable->printTable();
-	cout << "Load Ratio: " << hashTable->getLoadRatio() << endl;
+	//hashTable->printTable();
+        double totalInsertions = totalBuckets*hashTable->getLoadRatio();
+        double avgComparisons = HashTableBase::totalComparisons/totalInsertions;
+        cout << "Load Ratio: " << maxLoadRatio << " Average Comparisons: " << avgComparisons << endl;
 
-	return 0;
+	//return 0;
+        string response;
+        
+        
+        HashTableBase::totalComparisons = 0;
+}     
+    cout << "^^^^^Method: " << collisionResolutionScheme << endl << endl;
+    }
 }
-
 
 
 // <will>
@@ -143,3 +171,4 @@ int getIntegerFromUser()
 	}
 	return response;
 }
+
